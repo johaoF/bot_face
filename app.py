7ab74c2f1ea43791302a3a0d025768eb0370a73e
johaoF <123512952+johaoF@users.noreply.github.com>
2023-03-06 13:19:48 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def webhook():
+    if request.method == 'GET':
+        token = request.args.get('hub.verify_token')
+        challenge= request.args.get('hub.challenge')
+        if token == 'secret':
+            return str(challenge)
+        return '400'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
